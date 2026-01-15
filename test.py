e2418ce31ce168e789d2fe7d7037bd063845d2bd
Louis Stevens <louis@louisstevens.com>
2026-01-15 18:18:20 +0000

    No clue, apparently something to do with the test json

---
+++ test.py
@@ -0,0 +1,14 @@
+
+import json
+
+testJsonDict = {
+	'segment_data': [{
+		'sequential_id': '1631', # From which the uri can be derived
+		'author_name': "Bob's little toe",
+		'description': 'My carpet is a really cool shade of pink',
+		'title': 'My carpet is cool'
+	} for i in range (250)]
+}
+
+with open('test_for_loading.json', 'w') as jsonFile:
+	jsonFile.write(json.dumps(testJsonDict))
