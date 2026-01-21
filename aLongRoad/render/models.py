12f2df4daec6a7c4d9105e66d67b1b6edf17d9d5
Louis Stevens <louis@louisstevens.com>
2026-01-21 02:04:31 +0000

    Adds basic models and argon hashing test in base directory.

--- aLongRoad/render/models.py
+++ aLongRoad/render/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+
+class RoadSpace(models.Model):
+	def __str__(self) -> str:
+		return f'<Road Space No. {self.pk}>'
+
+
+class RoadSpaceReservation(models.Model):
+	roadSpace = models.OneToOneField(
+		RoadSpace,
+		on_delete=models.CASCADE,
+		related_name='reservation'
+	)
+
+	encryptedTokenHash = models.CharField(max_length=255)
+	
+	expiryTime = models.DateTimeField(auto_now=False, auto_now_add=False) 
+
+
+	def setExpiryTime(self):
+		pass
+		
+		# Time reservation made at + set time period (e.g. 1 week)
+
+
+class UserGeneratedRoadSegment(models.Model):
+	roadSpace = models.ForeignKey(RoadSpace, on_delete=models.SET_NULL, null=True) # Check difference between null and blank
+
+	# All lengths are placeholders:
+
+	title = models.CharField(max_length=200)
+	author = models.CharField(max_length=100)
+	description = models.CharField(max_length=400)
+
+	status = models.CharField(
+		max_length=50,
+		choices=[
+			('FR', 'for_review'),
+			('PB', 'published'),
+			('RJ', 'rejected')
+		]
+	)
